1285d02064d99d21ea379ef3410d9699d69d30ac
thrustMIT <team.thrustmit@manipal.edu>
2022-11-07 01:48:23 +0530

    Added Separate Gallery Page

---
+++ assets/js/pictures.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $(".fancybox").fancybox({
+          openEffect: "none",
+          closeEffect: "none"
+      });
+      
+      $(".zoom").hover(function(){
+          
+          $(this).addClass('transition');
+      }, function(){
+          
+          $(this).removeClass('transition');
+      });
+  });
+      
